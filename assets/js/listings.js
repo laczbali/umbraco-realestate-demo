83f76f290d6cdec3560ce655c52d0adda48dbeb3
Balazs Laczko <laczbali@gmail.com>
2022-08-04 15:53:22 +0200

    fixed a bug with the simplesearch pagination

--- assets/js/listings.js
+++ assets/js/listings.js
@@ -1,73 +1,56 @@
 window.onload = function () {
+    // run the listing-reveal "animation"
     revealCards();
+
+    // remove stored item from previous session
+    localStorage.removeItem("listings-query");
 }
 
 /**
- *  Goes through each listing card, and removes the opaque class from them, with a bit of a delay
+ * Gets the listings, at the requested page number, with the requested filters
+ * 
+ * - When called by the search button, it queries the first page, with the searchbox value
+ * - When called by the filter apply, it queries the first page, with the filter values
+ * - When called by the paginations, it queries the requested page, with the last used additional params
+ * 
+ * @param {number} pageNumber If null, the 1st page will be queried
+ * @param {string} filters Will be applied if not null 
+ * @param {boolean} reuseFilters If true, and filters is null, will re-use the last used filters
  * */
-function revealCards() {
-    const cards = Array.from(document.getElementsByClassName("card-fading-isopaque"));
-    cards.forEach(function (card, i) {
-        setTimeout(function () {
-            card.classList.remove("card-fading-isopaque");
-        }, i * 100);
-    });
-}
-
-/**
- * Gets a specific page of the listings
- * 
- * If the pageNumber is the same as the current page,
- * - assumes the filters have been updated
- * - resets the page to the first
- * - will also apply the filter input to the query (if any)
- * 
- * If the pageNumber is for a different page,
- * - collects the currently applied filter settings
- * - gets the desired page with those filters applied (if any)
- * 
- * @param {number} pageNumber
- * @param {string} filters What filters to apply, as an URL encoded query string (eg: "&key=value&key2=value2")
- */
-async function getPage(pageNumber, filters = null) {
+async function getListings(pageNumber = null, filters = null, reuseFilters = false) {
     const url = window.location.href;
     const urlParts = url.split("/");
     if (urlParts[urlParts.length - 1] === '') { urlParts.pop(); }
     // remove the last part of the url, as it points the list-view
     urlParts.pop();
     // add the part for the query controller
-    urlParts.push("listing-results")
+    urlParts.push("listing-results");
     const baseQueryUrl = urlParts.join("/");
 
-    // build query url
-    let queryUrl = baseQueryUrl
-    if (pageNumber !== currentPage()) {
-        // function was called by pagination
-        // collect active filters, and go to needed page
-        queryUrl = `${baseQueryUrl}?page=${pageNumber}`;
-        filters = collectFilters();
-    } else {
-        // function was called by applyFilters
-        // since filters changed, we want to reset to page 1
-        queryUrl = `${baseQueryUrl}?page=1`;
+    // add page number
+    if (pageNumber === null) { pageNumber = 1; }
+    let queryUrl = `${baseQueryUrl}?page=${pageNumber}`;
+
+    // add filters, if applicable
+    if (filters === null && reuseFilters) {
+        // no filters were provided, but we need to reuse the previous (pagination)
+        filters = localStorage.getItem("listings-query");
     }
     if (filters !== null) {
-        // function was either called by applyFilters, so they are provided
-        // or it was called through pagination, so it was collected
-        // if it is null, no filters are set by the user
+        // we have filters (either provided or reused)
+        localStorage.setItem("listings-query", filters);
         queryUrl += filters;
     }
 
-    // display results
+    // fetch & display results
     const result = await fetchAsyncText(queryUrl);
     const resultsContainer = document.getElementById("results-container");
     resultsContainer.innerHTML = result;
-
     revealCards();
 }
 
 /**
- * Builds a query param from the contents of the simple search field
+ * Queries the listings with the contents of the simple search field (1st page)
  * */
 function simpleSearch() {
     let queryUrl = null;
@@ -76,18 +59,29 @@ function simpleSearch() {
         queryUrl = `&searchterm=${searchBox.value}`;
         queryUrl = encodeURI(queryUrl);
     }
-    getPage(currentPage(), queryUrl)
+
+    getListings(null, queryUrl);
 }
 
 /**
- * Queries the listings with the currently applied filters. This will reset pagination to the first page.
+ * Queries the listings with the applied filters (1st page)
  * */
 function applyFilters() {
-    getPage(currentPage(), collectFilters());
+    getListings(null, collectFilters());
+}
+
+/**
+ * Gets the desired page, with the current parameters
+ * @param {any} pageNumber
+ */
+function getPage(pageNumber) {
+    getListings(pageNumber, null, true);
 }
 
 /**
  * Collect the input data from all the search filters (except the simple-search-textbox)
+ * Returns the URL encoded query string (" &region=North&tags=parking,pet-friendly ")
+ * Returns null, if no filters are specified
  * */
 function collectFilters() {
     // build a query string "&tags=a,b&regions=x,y&types=rent"
@@ -152,13 +146,15 @@ function collectFilters() {
 }
 
 /**
- * Returns which page we are currently on (based on the page-selector widget)
+ *  Goes through each listing card, and removes the opaque class from them, with a bit of a delay
  * */
-function currentPage() {
-    const activePageSelector = document.getElementsByClassName("page-selector-current")[0];
-    if (activePageSelector === undefined) { return "1"; }
-    const currentPageVal = activePageSelector.id.split("-")[1];
-    return currentPageVal;
+function revealCards() {
+    const cards = Array.from(document.getElementsByClassName("card-fading-isopaque"));
+    cards.forEach(function (card, i) {
+        setTimeout(function () {
+            card.classList.remove("card-fading-isopaque");
+        }, i * 100);
+    });
 }
 
 /**
