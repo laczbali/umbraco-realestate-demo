92ed0f6444f0edb7f2a6e608363d731157ac23be
Balazs Laczko <laczbali@gmail.com>
2022-08-16 16:55:32 +0200

    added custom routing pipeline for displaying listing items from the db

---
+++ Components/CustomRoutesComponent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Umbraco.Core;
+using Umbraco.Core.Composing;
+using Umbraco.Core.Services.Implement;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using Umbraco.Web.PublishedModels;
+using umbraco_realestate_demo.Database;
+using umbraco_realestate_demo.Database.Models;
+using Umbraco.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Umbraco.Core.Models.PublishedContent;
+
+namespace umbraco_realestate_demo.Components
+{
+    public class CustomRoutesComposer : ComponentComposer<CustomRoutesComponent> { }
+
+    public class CustomRoutesComponent : IComponent
+    {
+        public void Initialize()
+        {
+            RouteTable.Routes.MapRoute("Listing", "listing/{id}", new { controller = "Listing", action = "Get", id = UrlParameter.Optional });
+        }
+
+        public void Terminate() { }
+    }
+}
---
+++ Controllers/ListingController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Core;
+using Umbraco.Web;
+using Umbraco.Web.Mvc;
+
+namespace umbraco_realestate_demo.Controllers
+{
+	public class ListingController : RenderMvcController
+    {
+        public ActionResult Get(int id)
+        {
+            var listings = Umbraco.ContentAtXPath("//listings[@isDoc]").First();
+            ViewData["id"] = id;
+            return View("~/Views/DbListingItem.cshtml", listings);
+        }
+    }
+}
