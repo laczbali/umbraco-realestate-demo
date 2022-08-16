445963b0b86ad9a1bdc5134bde95d13f8e4a5a31
Balazs Laczko <laczbali@gmail.com>
2022-08-16 16:47:35 +0200

    fixed a few bugs in the publishing event handling

--- Components/PublishEventComponent.cs
+++ Components/PublishEventComponent.cs
@@ -16,16 +16,6 @@ namespace umbraco_realestate_demo.Components
 {
     public class PublishEventComponent : IComponent
     {
-        [RuntimeLevel(MinLevel = RuntimeLevel.Run)]
-        public class PublishEventComposer : ComponentComposer<PublishEventComponent>
-        { }
-
-        public void Initialize()
-        {
-            // subscribe at startup
-            ContentService.Publishing += ContentService_Publishing;
-        }
-
         private void ContentService_Publishing(Umbraco.Core.Services.IContentService sender, Umbraco.Core.Events.ContentPublishingEventArgs e)
         {
             var publishedListings = e.PublishedEntities.Where(node => node.ContentType.Alias == "listingItem");
@@ -34,24 +24,25 @@ namespace umbraco_realestate_demo.Components
                 using (var context = new ListingContext())
                 {
                     var item = context.ListingItems.FirstOrDefault(i => i.Name == node.Name);
-                    if(item == null)
+                    if (item == null)
                     {
-                       item = new Database.Models.ListingItem();
-                       context.ListingItems.Add(item);
+                        item = new Database.Models.ListingItem();
+                        context.ListingItems.Add(item);
                     }
 
                     item.Name = node.Name;
+                    item.CreateDate = node.CreateDate;
                     item.HeadLine = node.GetValue<string>("headline");
                     item.Description = node.GetValue<string>("description");
                     item.Contact = node.GetValue<string>("contact");
-                    item.Region = node.GetValue<string>("region");
-                    item.ListingType = node.GetValue<string>("listingType");
+                    item.Region = JsonConvert.DeserializeObject<string[]>(node.GetValue<string>("region"))[0];
+                    item.ListingType = JsonConvert.DeserializeObject<string[]>(node.GetValue<string>("listingType"))[0];
                     item.Price = node.GetValue<int>("price");
                     item.Size = node.GetValue<int>("size");
                     item.Rooms = node.GetValue<int>("rooms");
 
                     // remove tags that are not needed
-                    var nodeTags = JsonConvert.DeserializeObject<string[]>(node.GetValue<string>("tags"));
+                    var nodeTags = JsonConvert.DeserializeObject<string[]>(node.GetValue<string>("tags") ?? "[]");
                     item.Tags.RemoveAll(t => !nodeTags.Contains(t.Name));
                     // add tags, if missing
                     foreach (var tag in nodeTags)
@@ -70,10 +61,12 @@ namespace umbraco_realestate_demo.Components
                     }
 
                     // remove media that are not needed
-                    var nodeImages = JsonConvert.DeserializeAnonymousType(node.GetValue<string>("images"), new[] { new { mediaKey = "", key = "" } } );
+                    var nodeImages = JsonConvert.DeserializeAnonymousType(node.GetValue<string>("images"), new[] { new { mediaKey = "", key = "" } });
                     var helper = Umbraco.Web.Composing.Current.UmbracoHelper;
                     var nodeMedia = helper.Media(nodeImages.Select(m => m.mediaKey)).Select(m => m.Url());
-                    context.Media.RemoveRange(context.Media.Where(m => !nodeMedia.Contains(m.Filepath)));
+                    context.Media.RemoveRange(
+                        item.Media.Where(m => !nodeMedia.Contains(m.Filepath))
+                    );
                     // add media, if missing
                     foreach (var media in nodeMedia)
                     {
@@ -91,9 +84,18 @@ namespace umbraco_realestate_demo.Components
                     }
 
                     context.SaveChanges();
-
                 }
             }
+        }
+
+        [RuntimeLevel(MinLevel = RuntimeLevel.Run)]
+        public class PublishEventComposer : ComponentComposer<PublishEventComponent>
+        { }
+
+        public void Initialize()
+        {
+            // subscribe at startup
+            ContentService.Publishing += ContentService_Publishing;
         }
 
         public void Terminate()
